f9e13b532e14a150c4d09fef4248e60a8ccc8b4c
/nuvem <83615960+7zx@users.noreply.github.com>
2021-08-24 17:53:59 +0100

    Update winpcap.py

--- tools/addons/winpcap.py
+++ tools/addons/winpcap.py
@@ -1,4 +1,4 @@
-# Import modules
+# Importar modulos
 import os
 import sys
 import wget
@@ -9,13 +9,13 @@ if os.name == "nt":
     winpcap_dir = os.environ["ProgramFiles(x86)"] + "\\WinPcap"
     if not os.path.exists(winpcap_dir):
         print(
-            f'{Fore.MAGENTA}[!] {Fore.YELLOW}Attention! Component "WinPcap" not installed!\n    is needed to make an attack of syn, udp and others,\n    you can skip this if you want to use only SMS flood\n    Do you want to install it automatically? (y/n){Fore.RESET}'
+            f'{Fore.MAGENTA}[!] {Fore.YELLOW}Aviso! O componente "WinPcap" não foi instalado!\n    é necessario para fazer um ataque syn, udp e etc,\n    Queres instala-lo automaticamente? (y/n){Fore.RESET}'
         )
         if input(f"{Fore.MAGENTA} >>> {Fore.BLUE}").lower() in ("y", "yes", "1"):
-            print(f"{Fore.YELLOW}[~] {Fore.CYAN}Downloading installer...{Fore.BLUE}\n")
+            print(f"{Fore.YELLOW}[~] {Fore.CYAN}Instalando instalador...{Fore.BLUE}\n")
             winpcap_installer = wget.download(winpcap_url)
             os.startfile(winpcap_installer)
             print(
-                f"\n\n{Fore.GREEN}[?] {Fore.YELLOW}Now please restart program{Fore.RESET}"
+                f"\n\n{Fore.GREEN}[?] {Fore.YELLOW}reenicia o programa{Fore.RESET}"
             )
             sys.exit(1)
