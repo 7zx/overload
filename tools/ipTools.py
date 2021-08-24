636c0920fe39e1a769c3f2020a63ef4756b4ee3e
/nuvem <83615960+7zx@users.noreply.github.com>
2021-08-24 18:20:12 +0100

    Update ipTools.py

--- tools/ipTools.py
+++ tools/ipTools.py
@@ -49,3 +49,60 @@ def __GetURLInfo(target):
     if not target.startswith("http"):
         target = f"http://{target}"
     return target
+
+""" Get target, subject, body """
+
+
+def __GetEmailMessage():
+    server, username = ReadSenderEmail()
+    subject = input(f"{Fore.BLUE}[?] {Fore.MAGENTA}Enter the Subject (leave blank for random value): ")
+    body = input(f"{Fore.BLUE}[?] {Fore.MAGENTA}Enter Your Message (leave blank for random value): ")
+    return [server, username, subject, body]
+
+""" Return target """
+
+
+def GetTargetAddress(target, method):
+    if method == "SMS":
+        if target.startswith("+"):
+            target = target[1:]
+        return target
+    elif method == "EMAIL":
+        email = __GetEmailMessage()
+        email.append(target)
+        return email
+    elif method in (
+        "SYN",
+        "UDP",
+        "NTP",
+        "POD",
+        "MEMCACHED",
+        "ICMP",
+        "SLOWLORIS",
+    ) and target.startswith("http"):
+        parsed_uri = urlparse(target)
+        domain = "{uri.netloc}".format(uri=parsed_uri)
+        origin = socket.gethostbyname(domain)
+        __isCloudFlare(domain)
+        return origin, 80
+    elif method in ("SYN", "UDP", "NTP", "POD", "MEMCACHED", "ICMP", "SLOWLORIS"):
+        return __GetAddressInfo(target)
+    elif method == "HTTP":
+        url = __GetURLInfo(target)
+        __isCloudFlare(url)
+        return url
+    else:
+        return target
+
+
+""" Is connected to internet """
+
+
+def InternetConnectionCheck():
+    try:
+        requests.get("https://google.com", timeout=4)
+    except:
+        print(
+            f"{Fore.RED}[!] {Fore.MAGENTA}Your device is not connected to the Internet{Fore.RESET}"
+        )
+        sys.exit(1)
