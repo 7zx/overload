8ca7dba6e9aa663ba6d370b4db13cd743a0bfff2
/nuvem <83615960+7zx@users.noreply.github.com>
2021-08-24 17:58:05 +0100

    Update xor.py

--- tools/addons/twilight/xor.py
+++ tools/addons/twilight/xor.py
@@ -1,4 +1,4 @@
-# Import modules
+# Importa os modulos
 from base64 import b64encode
 from base64 import b64decode
 
@@ -11,10 +11,10 @@ def xor(text, key):
         out += chr(ord(current) ^ ord(current_key))
     return out
 
-# Encode : [text => xor => base64]
+# Codifica : [text => xor => base64]
 def encode(text, key):
     return b64encode(xor(text, key).encode("utf-8")).decode()
 
-# Decode : [xor => base64 => text]
+# Decodifica : [xor => base64 => text]
 def decode(text, key):
     return xor(b64decode(text).decode(), key)
