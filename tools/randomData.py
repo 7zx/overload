ce030df326e51174656b2d03104171773360c060
João Vitor <joaovitorsouza205@hotmail.com>
2022-10-02 15:33:27 -0300

    docs: translating it to english

--- tools/randomData.py
+++ tools/randomData.py
@@ -2,22 +2,22 @@ import json
 import random
 
 
-# Procura um IP aleatório
+# Searchs for a random IP
 def random_IP():
     ip = []
-    for _ in range(0, 4):
+    for _ in range(4):
         ip.append(str(random.randint(1, 255)))
     return ".".join(ip)
 
 
-# Procura uma referência aleatória
+# Searchs for a random referer
 def random_referer():
     with open("tools/L7/referers.txt", "r") as referers:
         referers = referers.readlines()
     return random.choice(referers)
 
 
-# Procura um user agent aleatório
+# Searchs for a random user agent
 def random_useragent():
     with open("tools/L7/user_agents.json", "r") as agents:
         user_agents = json.load(agents)["agents"]
