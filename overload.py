0d3caaab9d6716c452488a19c7446fe005cdef7f
João Vitor <joaovitorsouza205@hotmail.com>
2022-10-06 10:06:01 -0300

    refactor: added __name__ file condition

--- overload.py
+++ overload.py
@@ -1,7 +1,4 @@
 # -*- coding: utf-8 -*-
-
-# Created by nuvem and tsk
-
 import os
 import sys
 
@@ -9,40 +6,20 @@ from colorama import Fore
 
 # Changing CWD to the canonical path of the file.
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
+os.system("cls" if os.name == "nt" else "clear")
 
 try:
-    os.system("cls" if os.name == "nt" else "clear")
-
-    # Tries to download Wireshark if Windows OS is detected.
-    import tools.addons.wireshark
     from tools.addons.checks import check_number_input, check_target_input
+    from tools.addons.logo import show_logo
     from tools.method import AttackMethod
-
 except ImportError as err:
     from tools.crash import CriticalError
 
     CriticalError("Failed to import some packages", err)
 
 
-if __name__ == "__main__":
-
-    logo = """
-  ▒█████   ██▒   █▓▓█████  ██▀███   ██▓     ▒█████   ▄▄▄      ▓█████▄ 
-  ▒██▒  ██▒▓██░   █▒▓█   ▀ ▓██ ▒ ██▒▓██▒    ▒██▒  ██▒▒████▄    ▒██▀ ██▌
-  ▒██░  ██▒ ▓██  █▒░▒███   ▓██ ░▄█ ▒▒██░    ▒██░  ██▒▒██  ▀█▄  ░██   █▌
-  ▒██   ██░  ▒██ █░░▒▓█  ▄ ▒██▀▀█▄  ▒██░    ▒██   ██░░██▄▄▄▄██ ░▓█▄   ▌
-  ░ ████▓▒░   ▒▀█░  ░▒████▒░██▓ ▒██▒░██████▒░ ████▓▒░ ▓█   ▓██▒░▒████▓ 
-  ░ ▒░▒░▒░    ░ ▐░  ░░ ▒░ ░░ ▒▓ ░▒▓░░ ▒░▓  ░░ ▒░▒░▒░  ▒▒   ▓▒█░ ▒▒▓  ▒ 
-    ░ ▒ ▒░    ░ ░░   ░ ░  ░  ░▒ ░ ▒░░ ░ ▒  ░  ░ ▒ ▒░   ▒   ▒▒ ░ ░ ▒  ▒ 
-  ░ ░ ░ ▒       ░░     ░     ░░   ░   ░ ░   ░ ░ ░ ▒    ░   ▒    ░ ░  ░ 
-      ░ ░        ░     ░  ░   ░         ░  ░    ░ ░        ░  ░   ░    
-                ░                                               ░     
-  """
-
-    CRED2 = "\33[91m"
-    print(CRED2 + logo + CRED2)
-    print("├───DDOS TOOL LAYER 7")
-
+def main():
+    show_logo()
     try:
         time = check_number_input("time")
         threads = check_number_input("threads")
@@ -57,5 +34,7 @@ if __name__ == "__main__":
             f"\n{Fore.RED}[!] {Fore.MAGENTA}Ctrl+C detected. Program closed.{Fore.RESET}"
         )
         sys.exit(1)
-else:
-    sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
